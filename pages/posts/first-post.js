0e7c12921de76372d4707110464c16b7bc1b1916
slTrust <trustfor@sina.cn>
2021-01-30 16:51:25 +0800

    使用sass

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Link from 'next/link'
-import styles from 'styles/first-post.module.css'
+import styles from 'styles/first-post.module.scss'
 export default function x() {
   return (
     <>
       <div>First Post
       <hr />
         <Link href="/">Link回首页</Link>
-        <div className={styles.content}>1111</div>
+        <div className={styles.content}>
+          <p>1111</p>
+          2222
+        </div>
       </div>
     </>
 
