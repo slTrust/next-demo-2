89e4e7f76cbc3fb49f87c970f3156f783aa2845a
slTrust <trustfor@sina.cn>
2021-01-30 16:08:51 +0800

    Link快速导航

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import Link from 'next/link'
 export default function x() {
   return (
-    <div>First Post</div>
+    <div>First Post
+      <hr />
+      回到首页
+      <hr />
+      <a href="/">a标签回首页，会重新请求</a>
+      <hr />
+      <Link href="/">Link回首页</Link>
+    </div>
   )
 }
