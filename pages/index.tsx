46bbacef1b28115eee6192f177b7137952c23baf
slTrust <trustfor@sina.cn>
2021-01-30 17:47:08 +0800

    将next升级为ts

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import png from 'assets/images/003.png'
+export default function x() {
+  return (
+    <>
+      <div>home
+        <img src={png} />
+      </div>
+    </>
+
+  )
+}
