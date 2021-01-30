572a9f5d1ef4cc589b145205fe1516079b864ec9
slTrust <trustfor@sina.cn>
2021-01-30 17:16:50 +0800

    使用 next-images插件

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-module.exports = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.png$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[contenthash].[ext]',
-            publicPath: '/_next/static',
-            outputPath: 'static',
-          }
-        }
-
-      ]
-    })
-    return config;
+const withImages = require('next-images')
+module.exports = withImages({
+  webpack(config, options) {
+    return config
   }
-}
+})
