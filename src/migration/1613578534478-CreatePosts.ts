1a3046e7f642f39d24f2edd326ea065be2c1de29
slTrust <trustfor@sina.cn>
2021-02-18 00:40:50 +0800

    创建三个数据表 - users - posts - comments - 分别为每个表添加 createAt updatedAt

--- src/migration/1613578534478-CreatePosts.ts
+++ src/migration/1613578534478-CreatePosts.ts
@@ -1,20 +1,21 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateUsers1613578095866 implements MigrationInterface {
+export class CreatePosts1613578534478 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         return await queryRunner.createTable(new Table({
-            name:'users',
+            name:'posts',
             columns:[
                 {name:'id',isGenerated:true,type:'int',generationStrategy:'increment',isPrimary:true},
-                {name:'username',type:'varchar'},
-                {name:'password_digest',type:'varchar'},
+                {name:'title',type:'varchar'},
+                {name:'content',type:'varchar'},
+                {name:'author_id',type:'int'}
             ]
         }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        return await queryRunner.dropTable('users');
+        return await queryRunner.dropTable('posts');
     }
 
 }
