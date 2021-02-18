3f2087d53cbcb2d45cb24d3d701d896d4de36a50
slTrust <trustfor@sina.cn>
2021-02-18 18:54:05 +0800

    用 session['currentUser'] 记录登录用户 - cookie设置要 禁用secure，因为本地开发是 http 不是https - withSession 处理session操作 - 给 NextApiRequest 添加自定义的属性

---
+++ lib/withSession.tsx
@@ -0,0 +1,12 @@
+import {withIronSession} from 'next-iron-session';
+import {NextApiHandler} from 'next';
+
+export function withSession(handler: NextApiHandler) {
+    return withIronSession(handler, {
+        // password: process.env.SECRET_COOKIE_PASSWORD,
+        password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da', //密钥，这个应该用环境变量
+        cookieName: 'blog',
+        // 这个选项如果不设置 你本地开发是http的 就会种不下 cookie 获取 user 会是 undefined
+        cookieOptions: {secure: false}
+    });
+}
