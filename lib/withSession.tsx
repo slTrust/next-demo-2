a58eccff34192a84b4c39527be402433b968f7a0
slTrust <trustfor@sina.cn>
2021-02-18 19:09:52 +0800

    使用 .env.local 设置环境变量 - 密钥这种危险内容的最佳实践

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,8 +3,8 @@ import {NextApiHandler} from 'next';
 
 export function withSession(handler: NextApiHandler) {
     return withIronSession(handler, {
-        // password: process.env.SECRET_COOKIE_PASSWORD,
-        password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da', //密钥，这个应该用环境变量
+        // password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da', //密钥，这个应该用环境变量
+        password: process.env.SECRET, // 正确姿势是 用环境变量
         cookieName: 'blog',
         // 这个选项如果不设置 你本地开发是http的 就会种不下 cookie 获取 user 会是 undefined
         cookieOptions: {secure: false}
