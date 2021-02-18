5d506985500bca82522157b59e3e4778e597d02d
slTrust <trustfor@sina.cn>
2021-02-18 19:12:27 +0800

    消除一处 @ts-ignore

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -1,7 +1,7 @@
 import {withIronSession} from 'next-iron-session';
-import {NextApiHandler} from 'next';
+import {GetServerSideProps, NextApiHandler} from 'next';
 
-export function withSession(handler: NextApiHandler) {
+export function withSession(handler: NextApiHandler|GetServerSideProps) {
     return withIronSession(handler, {
         // password: 'c2a85490-cc60-4f21-94e8-8dc5dd3220da', //密钥，这个应该用环境变量
         password: process.env.SECRET, // 正确姿势是 用环境变量
