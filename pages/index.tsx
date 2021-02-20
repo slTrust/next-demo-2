dc9066fa609e0c9a476869e24d728c4370c0e501
slTrust <trustfor@sina.cn>
2021-02-20 08:08:39 +0800

    添加首页

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,28 @@
-import PostsIndex, {getServerSideProps} from './posts';
+import {NextPage} from 'next';
+import Link from 'next/link';
 
-export default PostsIndex;
 
-export {getServerSideProps};
+const Home: NextPage = () => {
+    return (
+        <>
+            <div className="cover">
+                <img src="/logo.png" alt=""/>
+                <h1>我的博客</h1>
+                <p>next.js搭建的博客</p>
+                <p><Link href="/posts"><a>文章列表</a></Link></p>
+            </div>
+       <style jsx>{`
+      .cover{
+        height: 100vh;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+      `}</style>
+        </>
+    );
+};
+
+export default Home;
+
