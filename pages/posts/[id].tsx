976df467766effca361157778e1e22dbbde87a06
slTrust <trustfor@sina.cn>
2021-02-20 08:49:18 +0800

    删除 blog

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {GetServerSideProps, GetServerSidePropsContext, NextPage} from 'next';
 import {getDatabaseConnection} from '../../lib/getDatabaseConnection';
 import {Post} from '../../src/entity/Post';
 import marked from 'marked';
 import Link from 'next/link';
 import {withSession} from '../../lib/withSession';
+import axios from 'axios';
+import {useRouter} from 'next/router';
 
 type Props = {
+    id: number;
     post: Post;
     currentUser: User | null;
 }
 const postsShow: NextPage<Props> = (props) => {
-    const {post, currentUser} = props;
+    const {post, currentUser, id} = props;
+    const router = useRouter()
+    const onRemove = useCallback(() => {
+        axios.delete(`/api/v1/posts/${id}`).then(() => {
+            window.alert('删除成功');
+            router.push('/posts')
+        }, () => {
+            window.alert('删除失败');
+        });
+    }, [id]);
     return (
         <>
             <div className="wrapper">
                 <header>
                     <h1>{post.title}</h1>
                     {currentUser &&
-                    <p>
+                    <p className="actions">
                         <Link href="/posts/[id]/edit" as={`/posts/${post.id}/edit`}><a>编辑</a></Link>
+                        <button className="deleteButton" onClick={onRemove}>删除</button>
                     </p>
                     }
                 </header>
@@ -27,12 +40,19 @@ const postsShow: NextPage<Props> = (props) => {
                 </article>
             </div>
             <style jsx>{`
+      .actions > *{
+        margin: 4px;
+      }
+      .actions > *:first-child{
+        margin-left: 0;
+      }
       .wrapper{
         max-width: 800px;
         margin: 16px auto;
         padding: 0 16px;
       }
       h1{padding-bottom: 16px; border-bottom: 1px solid #666;}
+      .deleteButton{color:red;background:transparent;border:none;}
       `}</style>
         </>
     );
@@ -41,14 +61,16 @@ const postsShow: NextPage<Props> = (props) => {
 export default postsShow;
 
 export const getServerSideProps: GetServerSideProps<any, { id: string }> = withSession(
-async (context: GetServerSidePropsContext) => {
-    const connection = await getDatabaseConnection();
-    const post = await connection.manager.findOne('Post', context.params.id);
-    const currentUser = (context.req as any).session.get('currentUser') || null;
-    return {
-        props: {
-            post: JSON.parse(JSON.stringify(post)),
-            currentUser
-        }
-    };
-});
+    async (context: GetServerSidePropsContext) => {
+        const connection = await getDatabaseConnection();
+        const id = context.params.id;
+        const post = await connection.manager.findOne('Post', id);
+        const currentUser = (context.req as any).session.get('currentUser') || null;
+        return {
+            props: {
+                id: parseInt(id.toString()),
+                post: JSON.parse(JSON.stringify(post)),
+                currentUser
+            }
+        };
+    });
